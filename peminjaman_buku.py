b28f89930b570ff53bc899355ed96a59857efb32
zaenalrfn <ZAOfficial24@gmail.com>
2024-01-10 21:12:27 +0700

    update code and  database

--- peminjaman_buku.py
+++ peminjaman_buku.py
@@ -1,174 +1,198 @@
 import mysql.connector
+from datetime import datetime
 from prettytable import PrettyTable
-from datetime import date
+import os
+
+os.system("clear")
 
 
-# Inisialisasi koneksi database
-def buat_koneksi():
-    return mysql.connector.connect(
-        host="localhost", user="root", password="", database="peminjaman_buku"
+def koneksi():
+    db = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="",
+        port=3306,
+        database="peminjaman_buku",
     )
+    return db
 
 
-# Fungsi untuk menutup koneksi dan cursor
-def tutup_koneksi(connection, cursor):
+def tutupKoneksi(konek, cursor):
     cursor.close()
-    connection.close()
+    konek.close()
 
 
-# Fungsi untuk menampilkan semua buku dari database
-def ambil_semua_buku_dari_db():
-    connection = buat_koneksi()
-    cursor = connection.cursor()
+# fungsi buku
+def tambah_buku():
+    konek = koneksi()
+    cursor = konek.cursor()
 
-    cursor.execute("SELECT * FROM buku")
-    result = cursor.fetchall()
+    id_buku = int(input("Masukkan id buku : "))
+    judul = input("Masukkan judu buku : ")
+    tahun_terbit = int(input("Masukkan tahun terbit buku : "))
+    penulis = input("Masukkan penulis buku : ")
 
-    buku_dari_db = []
-    for row in result:
-        buku_dari_db.append(
-            {
-                "Kode Buku": row[0],
-                "Judul": row[1],
-                "Penerbit": row[2],
-                "Pengarang": row[3],
-                "Tahun": row[4],
-            }
-        )
-
-    tutup_koneksi(connection, cursor)
-    return buku_dari_db
+    sql = """ INSERT INTO pinjam_buku (id_buku, judul, tahun_terbit, penulis) VALUES (%s,%s,%s,%s) """
+    val_input = (id_buku, judul, tahun_terbit, penulis)
+    cursor.execute(sql, val_input)
+    print(f"Buku berhasil disimpan {cursor.rowcount}")
+    konek.commit()
+    tutupKoneksi(konek, cursor)
 
 
-# Fungsi untuk menyimpan buku ke database
-def simpan_buku_ke_db(kd_buku, judul, penerbit, pengarang, tahun):
-    connection = buat_koneksi()
-    cursor = connection.cursor()
-
-    cursor.execute(
-        "INSERT INTO buku (kd_buku, judul, penerbit, pengarang, tahun) VALUES (%s, %s, %s, %s, %s)",
-        (kd_buku, judul, penerbit, pengarang, tahun),
-    )
-    connection.commit()
-
-    tutup_koneksi(connection, cursor)
-
-
-# Fungsi untuk menghapus buku dari database
-def hapus_buku_db(kode_buku):
-    connection = buat_koneksi()
-    cursor = connection.cursor()
-
-    cursor.execute("DELETE FROM buku WHERE kd_buku = %s", (kode_buku,))
-    connection.commit()
-
-    tutup_koneksi(connection, cursor)
-
-
-# Fungsi untuk menampilkan semua buku
-def tampilkan_semua_buku():
-    buku = ambil_semua_buku_dari_db()
-    if not buku:
-        print("Tidak ada buku tersedia.")
+def lihat_semua_buku():
+    konek = koneksi()
+    cursor = konek.cursor()
+    cursor.execute("SELECT * FROM pinjam_buku")
+    hasil = cursor.fetchall()
+    print("===================")
+    print("SEMUA BUKU YANG TERSEDIA")
+    if cursor.rowcount < 0:
+        print("Tidak ada data buku")
     else:
-        print("\n=== Semua Buku ===")
         table = PrettyTable()
-        table.field_names = ["Kode Buku", "Judul", "Penerbit", "Pengarang", "Tahun"]
-        table.align["Kode Buku"] = "l"
+        table.field_names = ["Id Buku", "Judul", "Tahun Terbit", "Penulis"]
+        table.align["Id Buku"] = "l"
         table.align["Judul"] = "l"
-        table.align["Penerbit"] = "l"
-        table.align["Pengarang"] = "l"
-        table.align["Tahun"] = "l"
-
-        for book in buku:
+        table.align["Tahun Terbit"] = "l"
+        table.align["Penulis"] = "l"
+        for data_buku in hasil:
             table.add_row(
                 [
-                    f"Kode Buku: {book['Kode Buku']}",
-                    f"Judul: {book['Judul']}",
-                    f"Penerbit: {book['Penerbit']}",
-                    f"Pengarang: {book['Pengarang']}",
-                    f"Tahun: {book['Tahun']}",
+                    f"{data_buku[0]}",
+                    f"{data_buku[1]}",
+                    f"{data_buku[2]}",
+                    f"{data_buku[3]}",
                 ]
             )
-
         print(table)
-        print("============================")
+        print("==========================================")
 
 
-# Fungsi untuk menambahkan buku
-def tambah_buku():
-    kd_buku = input("Masukkan kode buku : ")
-    judul = input("Masukkan judul buku: ")
-    penerbit = input("Masukkan penerbit buku: ")
-    pengarang = input("Masukkan pengarang buku: ")
-    tahun = input("Masukkan tahun terbit buku: ")
-
-    simpan_buku_ke_db(kd_buku, judul, penerbit, pengarang, tahun)
-    print(f"Buku '{judul}' berhasil ditambahkan.")
-
-
-# Fungsi untuk menghapus buku
-def hapus_buku():
-    buku = ambil_semua_buku_dari_db()
-    tampilkan_semua_buku()
-
-    # Ubah kode_buku menjadi string
-    kode_buku = input("Masukkan kode buku yang ingin dihapus: ")
-
-    # Periksa apakah kode buku ada di database sebelum menghapus
-    if any(str(book["Kode Buku"]) == kode_buku for book in buku):
-        hapus_buku_db(kode_buku)
-        print(f"Buku dengan kode {kode_buku} berhasil dihapus.")
-
-        # Perbarui variabel buku setelah penghapusan
-        buku = ambil_semua_buku_dari_db()
-        tampilkan_semua_buku()
+def peminjam():
+    konek = koneksi()
+    cursor = konek.cursor()
+    cursor.execute("SELECT * FROM peminjam")
+    hasil = cursor.fetchall()
+    if cursor.rowcount < 0:
+        print("Tidak ada data peminjam")
     else:
-        print(f"Buku dengan kode {kode_buku} tidak ditemukan.")
+        table = PrettyTable()
+        table.field_names = ["Id Peminjam", "Nama"]
+        table.align["Id Peminjam"] = "l"
+        table.align["Nama"] = "l"
+        for data_peminjam in hasil:
+            table.add_row(
+                [
+                    f"{data_peminjam[0]}",
+                    f"{data_peminjam[1]}",
+                ]
+            )
+        print(table)
 
 
-def tambah_peminjam():
-    connection = buat_koneksi()
-    cursor = connection.cursor()
-    nama = input("Masukkan nama Peminjam : ")
-    waktu = date.today()
-    nama_buku = input("Masukkan nama buku yang dipinjam : ")
-    kode_buku = input("Masukkan kode buku yang dipinjam : ")
-    cursor.execute(
-        """
-            INSERT INTO peminjam (ID, Nama, Waktu, nama_buku, kd_buku)
-            VALUES (%s, %s, %s, %s, %s)
-        """,
-        ("", nama, waktu, nama_buku, kode_buku),
-    )
-    connection.commit()
+def input_peminjam():
+    nama_peminjam = input("Masukkan nama peminjam buku : ")
+    tambah_peminjam(nama_peminjam)
 
 
-# Looping untuk interaksi dengan pengguna
+def tambah_peminjam(nama_peminjam):
+    konek = koneksi()
+    cursor = konek.cursor()
+    sql = "INSERT INTO peminjam (nama) VALUES (%s)"
+    input_val = (nama_peminjam,)
+    cursor.execute(sql, input_val)
+    konek.commit()
+    tutupKoneksi(konek, cursor)
+
+
+def buku_dipinjam():
+    konek = koneksi()
+    cursor = konek.cursor()
+    lihat_semua_buku()
+    idBuku = int(input("Masukkan id buku yang mau dipinjam : "))
+    peminjam()
+    idPeminjam = int(input("Masukkan id peminjam buku : "))
+    waktuSekarang = datetime.now()
+    waktu = waktuSekarang.date()
+    sql = "INSERT INTO peminjam_buku (Id_Buku, Id_Peminjam, waktu_Peminjaman) VALUES (%s,%s,%s)"
+    input_val = (idBuku, idPeminjam, waktu)
+    cursor.execute(sql, input_val)
+    konek.commit()
+    tutupKoneksi(konek, cursor)
+
+
+def cari_data_peminjam():
+    konek = koneksi()
+    cursor = konek.cursor()
+    peminjam()
+    id_peminjam = int(input("Masukkan id peminjam untuk mencari data : "))
+    sql = f"""
+        SELECT peminjam_buku.Id_Pinjam_Buku, peminjam.id_peminjam, peminjam.nama, pinjam_buku.id_buku, pinjam_buku.judul, pinjam_buku.penulis, pinjam_buku.tahun_terbit, peminjam_buku.waktu_Peminjaman 
+        FROM peminjam_buku
+        JOIN peminjam ON peminjam_buku.Id_Peminjam = peminjam.id_peminjam 
+        JOIN pinjam_buku ON peminjam_buku.Id_Buku = pinjam_buku.id_buku WHERE peminjam.id_peminjam = {id_peminjam};
+    """
+    cursor.execute(sql)
+    hasil = cursor.fetchall()
+    if cursor.rowcount < 0:
+        print("Tidak ada data peminjam")
+    else:
+        table = PrettyTable()
+        table.field_names = [
+            "Id Peminjaman",
+            "Id Peminjam",
+            "Nama",
+            "Id Buku",
+            "Judul",
+            "Penulis",
+            "Tahun Terbit",
+            "Waktu Meminjam",
+        ]
+        table.align["Id Peminjaman"] = "l"
+        table.align["Id Peminjam"] = "l"
+        table.align["Nama"] = "l"
+        table.align["Id Buku"] = "l"
+        table.align["Judul"] = "l"
+        table.align["Penulis"] = "l"
+        table.align["Tahun Terbit"] = "l"
+        table.align["Waktu Meminjam"] = "l"
+        for data_peminjam_buku in hasil:
+            table.add_row(
+                [
+                    f"{data_peminjam_buku[0]}",
+                    f"{data_peminjam_buku[1]}",
+                    f"{data_peminjam_buku[2]}",
+                    f"{data_peminjam_buku[3]}",
+                    f"{data_peminjam_buku[4]}",
+                    f"{data_peminjam_buku[5]}",
+                    f"{data_peminjam_buku[6]}",
+                    f"{data_peminjam_buku[7]}",
+                ]
+            )
+        print(table)
+
+
 while True:
-    print("============================")
-    print("| SISTEM PEMINJAMAN BUKU   |")
-    print("============================")
-    print("|1|. Tambah Peminjam Buku    |")
-    print("|2|. Tampilkan Data Peminjam |")
-    print("|3|. Tampilkan Semua Buku     |")
-    print("|4|. Tambah Buku             |")
-    print("|5|. Hapus Buku              |")
-    print("|0|. Keluar                  |")
-    print("============================")
+    print("SELAMAT DATANG DI SISTEM PEMINJAMAN")
+    print("============ Menu ============")
+    print("1. Tambah buku")
+    print("2. Lihat semua buku")
+    print("3. Tambah peminjam")
+    print("4. Masukkan buku yang di pinjam")
+    print("5. Cari data peminjam")
+    print("0. keluar")
+    print("===============================")
 
-    pilihan = input("Masukkan pilihan Anda (0-3): ")
+    pilihan = int(input("Masukkan menu yang anda pilih (0-4) : "))
 
-    if pilihan == "1":
-        tambah_peminjam()
-    elif pilihan == "2":
-        tampilkan_semua_buku()
-    elif pilihan == "3":
+    if pilihan == 1:
         tambah_buku()
-    elif pilihan == "4":
-        hapus_buku()
-    elif pilihan == "0":
-        print("Terima kasih, sampai jumpa!")
-        break
-    else:
-        print("Pilihan tidak valid. Silakan coba lagi.")
+    elif pilihan == 2:
+        lihat_semua_buku()
+    elif pilihan == 3:
+        input_peminjam()
+    elif pilihan == 4:
+        buku_dipinjam()
+    elif pilihan == 5:
+        cari_data_peminjam()
